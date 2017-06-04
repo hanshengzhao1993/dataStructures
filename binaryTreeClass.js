bd3440543cf771658306e489d351f88b8070d828
Han <hanshengzhao1993@gmail.com>
2017-06-04 11:19:39 -0700

    Added Breadth first search

--- binaryTreeClass.js
+++ binaryTreeClass.js
@@ -9,19 +9,56 @@ class BinaryTree{
 
   push(value){
   let currentNode = this;
+
   if(value < currentNode.value){
     if(currentNode.left === null){
-      currentNode.left = new Node(value)
+      currentNode.left = new BinaryTree(value)
     } else {
-      console.log('what is current left node:  ',currentNode.left);
       currentNode.left.push(value)
     }
-  } 
-  if(value > currentNode.value){
+  } else if (value > currentNode.value){
     if(currentNode.right === null){
-      currentNode.right = new Node(value)
+      currentNode.right = new BinaryTree(value)
     } else {
       currentNode.right.push(value)
+    }
+  } else {
+    return "Number Already Used"
+  }
+ }
+
+ containsVal(value){
+  let currentNode = this;
+  if(currentNode.value === value){
+    return true;
+  }
+  if(currentNode.value > value){
+    if(currentNode.left === null){
+      return false;
+    }
+    return currentNode.left.containsVal(value)
+  }
+
+  if(currentNode.value < value){
+    if(currentNode.right === null){
+      return false;
+    }
+    return currentNode.right.containsVal(value)
+  }
+ }
+ breadthFirstSearch(cb){
+  var queue = [];
+  queue.push(this);
+  while(queue.length){
+    var node = queue.shift();
+    if(node){
+      cb(node.value)
+    }
+    if(node.left){
+      queue.push(node.left)
+    }
+    if (node.right){
+      queue.push(node.right)
     }
   }
  }
@@ -29,20 +66,14 @@ class BinaryTree{
 }
 
 
-class Node{
-  constructor(value){
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-};
-
-
-
 let binaryTree = new BinaryTree(5);
 binaryTree.push(3)
 binaryTree.push(7)
 binaryTree.push(2)
+binaryTree.push(4)
+binaryTree.push(6)
+binaryTree.push(8)
 
-console.log(binaryTree);
-console.log(binaryTree.left);
+// console.log(binaryTree);
+console.log(binaryTree.breadthFirstSearch((ele)=>{console.log(ele)}))
+// console.log(binaryTree.containsVal(3))
