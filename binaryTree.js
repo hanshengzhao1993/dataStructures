6d41f5b7174fdaaba03a14122e913beecd728cd9
Han <hanshengzhao1993@gmail.com>
2017-06-26 17:47:33 -0700

    implemented Iterative binary treer

--- binaryTree.js
+++ binaryTree.js
@@ -102,6 +102,23 @@ BinaryTree.prototype.iterativeInOrderTraversal = function (cb, node) {
   }
 }
 
+BinaryTree.prototype.interativeInOrder = function (cb) {
+  var node = this;
+  var stack = [];
+  while (stack.length || node){
+    if(node){
+      stack.push(node);
+      node = node.left;
+    } else {
+      var popped = stack.pop();
+      cb(popped.value)
+      if(popped.right){
+        stack.push(popped.right)
+      }
+    }
+  }
+}
+
 var tree1 = new BinaryTree(1);
 tree1.left = new BinaryTree(5);
 tree1.right = new BinaryTree(2);
@@ -113,7 +130,7 @@ tree1.left.right = new BinaryTree(7);
 // tree1.right.left = node(6);
 // tree1.right.right.right = node(4);
 console.log(JSON.stringify(tree1, null , 2));
-console.log(tree1.iterativeInOrderTraversal( (ele)=>{console.log(ele)} ))
+console.log(tree1.interativeInOrder( (ele)=>{console.log(ele)} ))
 // console.log(tree1.postOrderTraversal( (ele)=>{console.log(ele)} ))
 
 // console.log(tree1.maxContinousSum())
