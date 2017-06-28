a783af4b231a163a08b7b2765cee26da0665cb93
Han <hanshengzhao1993@gmail.com>
2017-06-27 22:50:39 -0700

    implemented Divide

---
+++ divide.js
@@ -0,0 +1,17 @@
+// Divide two integers without using multiplication, division and mod operator.
+
+
+var divide = var divide = function(dividend, divisor) {
+  if (divisor === 0) {
+    return Number.MAX_VALUE;
+  }
+  
+  if (dividend === 0) {
+    return 0;
+  }
+
+  
+}
+
+
+console.log( divide(7,2) );
