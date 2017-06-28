24bcad26f10fe1c11f67262fdc2a73e82849b5b6
Han <hanshengzhao1993@gmail.com>
2017-06-28 15:13:03 -0700

    implemented LongestSUbstring

--- longestSubstring.js
+++ longestSubstring.js
@@ -3,13 +3,14 @@ var lengthOfLongestSubstring = function(str) {
   var largestSubString = '';
   var currentString = '';
 
-  for(var i = 0; i< str; i++){
+  for(var i = 0; i< str.length; i++){
+    // console.log('CURRENT STIRN: ', currentString)
     if(!currentString.includes(str[i])){
       currentString = currentString + str[i]
     } else {
       if(currentString.length > largestSubString.length){
         largestSubString = currentString;
-        currentString = '';
+        currentString = str[i];
       }
     }
   }
