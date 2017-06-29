fb65387afe10d030c091a981fe448229f7e5779c
Han <hanshengzhao1993@gmail.com>
2017-06-29 15:17:43 -0700

    Implemented reverse Number

--- reverseNumber.js
+++ reverseNumber.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var reverse = function(x) {
-
+  if(x >= 0){
+    return parseInt(x.toString().split('').reverse().join(''))
+  } else {
+    return parseInt('-'+x.toString().split('').reverse().join(''))
+  }
 };
+
+console.log('123',reverse(123))
+
+
+console.log('-123',reverse(-123))
+
+console.log('0',reverse(0))
