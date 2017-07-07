70685fce0a62980b4bf6daaed764cb429c998adb
Han <hanshengzhao1993@gmail.com>
2017-07-06 21:38:45 -0700

    implmented delete

--- linkedList.js
+++ linkedList.js
@@ -43,12 +43,51 @@ SinglyList.prototype.find = function (value) {
   }
 }
 
-SinglyList.prototype.remove = function (value){
-  var currentNode = this.head;
-  if(value = currentNode.value){
-    this.head = currentNode.next;
-  }
-}
+// SinglyList.prototype.remove = function (value){
+//   var currentNode = this.head;
+//   if(value = currentNode.value){
+//     this.head = currentNode.next;
+//   }
+// }
+
+SinglyList.prototype.remove = function(position) {
+    var currentNode = this.head,
+        length = this._length,
+        count = 0,
+        message = {failure: 'Failure: non-existent node in this list.'},
+        beforeNodeToDelete = null,
+        nodeToDelete = null,
+        deletedNode = null;
+ 
+    // 1st use-case: an invalid position
+    if (position < 0 || position > length) {
+        throw new Error(message.failure);
+    }
+ 
+    // 2nd use-case: the first node is removed
+    if (position === 1) {
+        this.head = currentNode.next;
+        deletedNode = currentNode;
+        currentNode = null;
+        this._length--;
+         
+        return deletedNode;
+    }
+ 
+    // 3rd use-case: any other node is removed
+    while (count < position) {
+        beforeNodeToDelete = currentNode;
+        nodeToDelete = currentNode.next;
+        count++;
+    }
+ 
+    beforeNodeToDelete.next = nodeToDelete.next;
+    deletedNode = nodeToDelete;
+    nodeToDelete = null;
+    this._length--;
+ 
+    return deletedNode;
+};
 
 
 var linkedList = new SinglyList();
