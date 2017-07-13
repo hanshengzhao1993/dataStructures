eda129a9ee6a966ee6546ff05dd0d7e7eef117ab
Han <hanshengzhao1993@gmail.com>
2017-07-13 09:29:08 -0700

    Made single linked list to test

--- printLinkedList.js
+++ printLinkedList.js
@@ -1,9 +1,14 @@
 function Node(data) {
-    this.data = data;
-    this.next = null;
+  this.data = data;
+  this.next = null;
 }
 
 function SinglyList() {
-    this._length = 0;
-    this.head = null;
+  this.head = null;
 }
+
+var linkedList = new SinglyList();
+linkedList.head = new Node(3);
+linkedList.head.next = new Node(5);
+linkedList.head.next.next = new Node(7);
+console.log(JSON.stringify(linkedList,null, 2))
