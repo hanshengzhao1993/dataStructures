4335801150e8c61f0f1a991c83cfd903dfc48467
Han <hanshengzhao1993@gmail.com>
2017-07-16 20:29:47 -0700

    implemented Happy Number

--- happyNumber.js
+++ happyNumber.js
@@ -6,5 +6,12 @@ var isHappy = function(n) {
   var a = JSON.stringify(n);
   while(a !== 1){
     var array = a.split('');
+    var temp = 0;
+    for(var i = 0; i< array.length; i++){
+      temp = temp + (parseInt(array[i]) * parseInt(array[i]));
+    }
+    if(temp === 1){
+      return true
+    }
   }
 };
