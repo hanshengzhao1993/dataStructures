bb49f1889d7931db6243dd00a0f6d92e98715c05
Han <hanshengzhao1993@gmail.com>
2017-07-21 12:25:21 -0700

    added patternsearch

---
+++ patternSearch.js
@@ -0,0 +1,13 @@
+var string = 'ABCDDCDABCDABCDDDCDA';
+var inputString = 'AB*D';
+
+
+let patternSearch = function (string, subString) {
+  let count = 0;
+
+  for(var i = 0; i< string.length; i++){
+    if(string[i] === subString[0]){
+      // first letters are the same now check it
+    }
+  }
+} 
