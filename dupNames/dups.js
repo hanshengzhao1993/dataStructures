3d30d11ec0a13b7bff9783ed3b68a0f97f8a7f7e
Han <hanshengzhao1993@gmail.com>
2017-07-22 00:30:45 -0700

    implemented Dupsjs

--- dupNames/dups.js
+++ dupNames/dups.js
@@ -7,7 +7,18 @@ fs.readFile('dups.txt', 'utf8', function (err, data) {
     var splitEle = ele.split(':');
     return splitEle;
   })
+  var obj = {};
+
+  splitDataAgain.forEach( (ele)=>{
+    if(obj[ele[1]] === undefined){
+      obj[ele[1]] = {
+        data: ele,
+        count: Object.keys(obj).length
+      }
+    }
+  });
   console.log(splitDataAgain);
+  console.log(obj)
 })
 
 // console.log(fs);
