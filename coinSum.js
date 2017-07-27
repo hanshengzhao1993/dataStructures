66482bf5056b3166176b2fe957e83db4865f9f3f
Han <hanshengzhao1993@gmail.com>
2017-07-27 10:46:10 -0700

    implemented coinSum

--- coinSum.js
+++ coinSum.js
@@ -6,5 +6,17 @@
 
  // Start with sorted coins
 var coinChange = function(coins, amount) {
-    
+  var copiedSum = amount;
+  var count = 0;
+  for(var i = coins.length; i> -1 ; i--) {
+    // console.log(i)
+    while(coins[i] <= copiedSum){
+      copiedSum = copiedSum - coins[i]
+      count++;
+    }
+  }
+  return count;
 };
+
+
+console.log(coinChange([1,2,5], 11))
