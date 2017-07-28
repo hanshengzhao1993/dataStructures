30386be30b56da73b8aff966d9377c8c4eb3dc21
Han <hanshengzhao1993@gmail.com>
2017-07-28 13:34:32 -0700

    implemented Unique using another data structure

--- allUnique.js
+++ allUnique.js
@@ -1,5 +1,15 @@
 'use strict'
 
 const unique = (string)=>{
-
+  var obj = {};
+  for(var i = 0; i< string; i++){
+    if(obj[string[i]] === undefined){
+      obj[string[i]] = string[i];
+    } 
+    else {
+      return false;
+    }
+  }
+  return true;
 }
+
