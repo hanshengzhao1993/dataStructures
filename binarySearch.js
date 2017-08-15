238e6b4c7ec55398dfc75a26a234b34b757c3f66
Han <hanshengzhao1993@gmail.com>
2017-08-14 21:25:20 -0700

    implemented function

--- binarySearch.js
+++ binarySearch.js
@@ -3,19 +3,22 @@
 var testArray = [1,2,3,4,5,6,7,8,9]
 
 var binarySearch = function (array, value, max, min) {
-  max = max || array.length -1 ;
+  max = max || array.length -1;
   min = min || 0;
   var middle = Math.floor((max - min)/2);
+  console.log(min, middle, max)
 
   if( array[middle] === value ){
     return middle;
   }
   if( array[middle] > value){
     return binarySearch(array, value, middle, min)
-  } else {
-    return binarySearch(array, value, max, middle)
+  } 
+  if(array[middle] < value) {
+    // console.log('went right', max, min, middle)
+    return binarySearch(array, value, max, middle);
   }
 
 }
 
-console.log(binarySearch(testArray))
+console.log(binarySearch(testArray, 8));
