b221beeec1678ea87cf5a1c7e0dc82fb5f880c58
Han <hanshengzhao1993@gmail.com>
2017-08-14 21:01:56 -0700

    changed input places

--- binarySearch.js
+++ binarySearch.js
@@ -2,7 +2,7 @@
 
 var testArray = [1,2,3,4,5,6,7,8,9]
 
-var binarySearch = function (array, max, min, value) {
+var binarySearch = function (array, value, max, min) {
   max = max || array.length -1 ;
   min = min || 0;
   var middle = Math.floor((max - min)/2);
@@ -11,9 +11,11 @@ var binarySearch = function (array, max, min, value) {
     return middle;
   }
   if( array[middle] > value){
-    return binarySearch(array, middle, min, value)
+    return binarySearch(array, value, middle, min)
   } else {
-    return binarySearch(array, max, middle, value)
+    return binarySearch(array, value, max, middle)
   }
 
 }
+
+console.log(binarySearch(testArray))
