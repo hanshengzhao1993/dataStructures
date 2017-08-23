c441a037f24afcb02d2ae85762972aad923c93d2
Han <hanshengzhao1993@gmail.com>
2017-08-22 20:51:33 -0700

    Added files

---
+++ linkedList/singleLinked/reverseLinkedList.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+
+const reverseLinkedList = (linkedList)=>{
+
+}
---
+++ linkedList/singleLinked/singlelyLinkedList.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class LinkedList{
+  constructor(data){
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SinglelyLinkedList {
+  constructor(data){
+    let node = new LinkedList(data);
+    this.head = node;
+    this.length = 0;
+  }
+  add(data){
+    let node = new LinkedList(data);
+    let currentNode = this.head;
+
+    if(!this.head){
+      this.head = node;
+      this.length++;
+      return node;
+    }
+
+    while(currentNode.next){
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = node;
+    this.length++;
+
+    return node;
+  }
+}
+
+const linkedList = new SinglelyLinkedList(1);
+linkedList.add(2)
+linkedList.add(3)
+
+console.log(linkedList);
+console.log(JSON.stringify(linkedList, null, 2));
