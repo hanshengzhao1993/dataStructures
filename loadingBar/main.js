b69097bc4ce6a64fd3e22c712d53d40a02f56f99
Han <hanshengzhao1993@gmail.com>
2017-09-09 21:51:19 -0700

    added logic for loading bar

--- loadingBar/main.js
+++ loadingBar/main.js
@@ -1,7 +1,15 @@
 var loadingBar = document.getElementById('progressBar');
 var loadingBarPercentage = 0;
-
+var loadingTime = 3000; // 3 sec
+var intervalTime = 100;
+var everyInterval = (intervalTime/ loadingTime) * 100;
+var startingInterval = setInterval( func, intervalTime);
 function func(){
-  
+  loadingBarPercentage += everyInterval;
+  var percentageLoading = loadingBarPercentage + '%';
+  loadingBar.style.width = percentageLoading;
+  if( loadingBarPercentage >= 100 ){
+    loadingBar.style.width = '100%';
+    clearInterval(startingInterval)
+  }
 }
-var startingInterval = setInterval( func, 1000);
