586ce556d45cf0b68dec87548035e5f8541359d2
Han <hanshengzhao1993@gmail.com>
2017-09-12 22:51:06 -0700

    implemented max heap

--- maxheap.js
+++ maxheap.js
@@ -36,9 +36,8 @@ MaxHeap.prototype.deleteMax = function () {
   var currentIndex = 0;
   var leftChild = (currentIndex * 2) + 1;
   var rightChild = (currentIndex * 2) + 2;
-  console.log('IN DELETE', this.heap)
-  var maxChild = this.heap[leftChild] > this.heap[rightChild] ? leftChild : rightChild;
-  while( this.heap[currentIndex] > this.heap[leftChild] || this.heap[rightChild] ){
+  while( this.heap[currentIndex] > this.heap[leftChild] || this.heap[currentIndex] > this.heap[rightChild] ){
+    var maxChild = this.heap[leftChild] > this.heap[rightChild] ? rightChild : leftChild;
     swap(this.heap, currentIndex, maxChild);
     currentIndex = maxChild;
     leftChild = (currentIndex * 2) + 1;
@@ -49,20 +48,42 @@ MaxHeap.prototype.deleteMax = function () {
   this.count --;
   return deletedValue;
 }
+MaxHeap.prototype.peak = function () {
+  return this.heap[0]
+}
 
 var heap = new MaxHeap();
-heap.insert(4);
-heap.insert(5);
-heap.insert(6);
-heap.insert(8);
-heap.insert(9);
-heap.insert(2);
-heap.insert(1);
+// heap.insert(4);
+// heap.insert(5);
+// heap.insert(6);
+// heap.insert(8);
+// heap.insert(9);
+// heap.insert(2);
+// heap.insert(1);
 heap.insert(11);
+heap.insert(12);
+heap.insert(13);
 console.log(heap.deleteMax())
+// console.log(heap.deleteMax())
+// console.log(heap.peak())
+// console.log('COUNT', heap.count)
 
 console.log(heap)
 
-// var a = [1,2,3,4];
-// swap(a, 1, 2)
-// console.log(a);
+var minNthValues = function (array, nth) {
+  var heap = new MaxHeap();
+  array.forEach( (ele, idx)=>{
+    if(idx < nth){
+      heap.insert(ele);
+    } else {
+      if(heap.peak() >= ele){
+        heap.deleteMax();
+        heap.insert(ele);
+      }
+    }
+    console.log('CURENT HEAP AFTER EACH ', heap);
+  })
+  return heap;
+}
+
+console.log('ANSWER', minNthValues([11,12,13,5, 49, 7,1], 3)) // 5 , 7, 8
