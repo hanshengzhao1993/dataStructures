f3c5ba8c1337fd265bc8d16ca4983294e08cf069
Han <hanshengzhao1993@gmail.com>
2017-09-17 21:34:04 -0700

    implemented addVertices

--- graph.js
+++ graph.js
@@ -1,5 +1,24 @@
+// current making an undirected graph
 var Graph = function () {
   this.vertices = [];
   this.edges = [];
   this.numberOfEdges = 0;
+};
+
+Graph.prototype.addVertices = function (value) {
+  this.vertices.push(value);
+  this.edges[value] = [];
+};
+
+Graph.prototype.addEdge = function (val1, val2) {
+  if( this.vertices.includes(val1) && this.vertices.includes(val2) ){
+
+  } else {
+    console.log('graph does not have one or both vertices')
+  }
 }
+
+var newGraph = new Graph();
+newGraph.addVertices(1);
+newGraph.addVertices(4);
+console.log(newGraph.addEdge(1,2))
