d221829f1802bd89ead88cf6589c505680dcbeab
Han <hanshengzhao1993@gmail.com>
2017-09-18 10:01:51 -0700

    finished implementing binary tree next value

--- binaryTreeNext.js
+++ binaryTreeNext.js
@@ -3,26 +3,24 @@ var BinaryTree = function (value) {
   this.left = this.right = null;
 };
 
-BinaryTree.prototype.setNext = function (cb) {
-  this.next = null;
+BinaryTree.prototype.setNext = function () {
   var currenNode = this;
   var queue = [this, null];
   while(queue.length){
     var firstNode = queue.shift();
-    console.log(firstNode, queue, this);
-    if(firstNode === null && queue[0] === null){
-      return;
-    }
     if(firstNode === null){
       queue.push(null);
+      if(queue[0] === null){
+        return;
+      }
     } else {
-    if(firstNode.left){
-      queue.push(firstNode.left)
-    }
-    if(firstNode.right){
-      queue.push(firstNode.right)
-    }
-    firstNode.next = queue[0];
+      if(firstNode.left){
+        queue.push(firstNode.left)
+      }
+      if(firstNode.right){
+        queue.push(firstNode.right)
+      }
+      firstNode.next = queue[0];
     }
   }
 }
@@ -35,3 +33,4 @@ newTree.left.left = new BinaryTree(2);
 newTree.right.right.left = new BinaryTree(11);
 
 console.log(newTree.setNext());
+console.log(newTree);
