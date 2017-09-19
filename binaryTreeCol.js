0c56764d9186dba30a15b84f00ac0de327a7176c
Han <hanshengzhao1993@gmail.com>
2017-09-19 15:19:52 -0700

    implmented binary tree col

--- binaryTreeCol.js
+++ binaryTreeCol.js
@@ -3,19 +3,23 @@ var BinaryTree = function (value) {
   this.left = this.right = null;
 };
 
-BinaryTree.prototype.findCol = function (val,count) {
+BinaryTree.prototype.findCol = function (count,obj) {
   count = count || 0;
-  if(this.left){
-    this.left.findCol(val, ++count);
+
+  if(obj[count] === undefined){
+    obj[count] = [this.value];
+  } else {
+    obj[count].push(this.value);
   }
 
-  if(this.value === val){
-    return count;
+  if(this.left){
+    this.left.findCol(count - 1, obj);
   }
 
   if(this.right){
-    this.right.findCol(val, --count);
+    this.right.findCol(count + 1, obj);
   }
+  return obj;
 }
 
 
@@ -25,3 +29,5 @@ newTree.right = new BinaryTree(10);
 newTree.right.right = new BinaryTree(12);
 newTree.left.left = new BinaryTree(2);
 newTree.right.right.left = new BinaryTree(11);
+
+console.log(newTree.findCol(0, {}));
