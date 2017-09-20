2f67a3c2317f486d225695588e8b4278e9cbd31d
Han <hanshengzhao1993@gmail.com>
2017-09-19 22:06:55 -0700

    implementing debounce

--- debounce.js
+++ debounce.js
@@ -4,39 +4,60 @@ var log = function (name) {
 
 
 var debounce = function (func, wait) {
-  var timeout;
-  return function () {
-    var args = arguments, context = this;
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      func.apply(context, args)
-    }, wait);
-  }
 }
 
 
 
-var debounceImmediate = function (func, wait, immediate) {
-  var timeout;
-  return function () {
-    var args = arguments,context = this;
-    var later = function () {
-      timeout = null;
-      if(!immediate){
-        func.apply(context, args);
-      }
-    }
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if(callNow){
-      func.apply(context, args)
-    }
-  }
-} 
 
-var testingDebounce = debounceImmediate(log, 200);
+var testingDebounce = debounce(log, 200);
 testingDebounce('Han')
 testingDebounce('Emily')
 testingDebounce('Ngu')
 testingDebounce('Hercules') 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var debounceImmediate = function (func, wait, immediate) {
+//   var timeout;
+//   return function () {
+//     var args = arguments,context = this;
+//     var later = function () {
+//       timeout = null;
+//       if(!immediate){
+//         func.apply(context, args);
+//       }
+//     }
+//     var callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if(callNow){
+//       func.apply(context, args)
+//     }
+//   }
+// } 
+
