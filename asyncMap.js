d75c23789db42fc3dfecc565f8e3de00e5941dfd
Han <hanshengzhao1993@gmail.com>
2017-09-20 17:21:19 -0700

    added async map with for loop

--- asyncMap.js
+++ asyncMap.js
@@ -1,24 +1,32 @@
 var func1 = function(cb) { // Make AJAX call.
   setTimeout(function() { // Wait for the internet.
     cb({name: 'Bob'}); // Return response.
-  }, 400);
+  }, 40);
 };
 
 var func2 = function(cb) {
   setTimeout(function() {
     cb({name: 'Jim'});
-  }, 200);
+  }, 20);
 };
 
 var func3 = function(cb) {
   setTimeout(function() {
     cb({name: 'Eli'});
-  }, 300);
+  }, 30);
 };
 
 
 var asyncMap = function (array,cb) {
- 
+  var answer = [];
+  for(var i = 0; i< array.length; i++){
+    array[i]( (value)=>{
+      answer.push(value);
+      if(answer.length === array.length){
+        cb(answer);
+      }
+    })
+  }
 }
 
 
